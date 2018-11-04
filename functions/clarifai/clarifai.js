717e397fbb47888a1d78154272781c9ca799f54a
Brian Kilrain <bkilrain@gmail.com>
2018-11-03 18:36:56 -0700

    Create Build Script for lambda functions
    Tweak clarifai function for POC
    
    Stringify Response body
    
    testing dev deployment
    
    Send file to lambda
    
    Test aws-sdk
    
    try different credentials
    
    save
    
    Use image upload service
    
    Update cloudformation deployment
    
    Update which util function is called
    
    Upload file to bucket with presigned url
    
    Move upload request to lambda
    
    debug
    
    debug
    
    try moving upload to clarifai function
    
    add axios
    
    check event object
    
    again
    
    boo auto format
    
    wtf
    
    event object
    
    change axios method
    
    url is being returned from serverless
    
    stringify axios response
    
    debug axios
    
    change case of post
    
    wtf again
    
    remove async tag
    
    double stringify
    
    abandon s3
    
    parse file for bytes
    
    wtf again and again
    
    add slash
    
    c'mon man!
    
    I almost give up
    
    please
    
    ok
    
    fudge
    
    boo
    
    bah
    
    boo
    
    move down
    
    hit api
    
    go go go
    
    please
    
    blah

--- functions/clarifai/clarifai.js
+++ functions/clarifai/clarifai.js
@@ -1,15 +1,22 @@
 const Clarifai = require("clarifai");
 
 const predict = (event, context, callback) => {
+  const file = event.body
+  
   const app = new Clarifai.App({
     apiKey: process.env.CLARIFAI_KEY
   });
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: { here: "here", api: process.env.CLARIFAI_KEY }
-    })
-  });
-};
 
+  app.inputs.search({ input: { base64: file } }).then(
+    function (response) {
+      callback(null, { statusCode: 200, body: JSON.stringify(response)})
+    },
+    function (err) {
+      callback(err)
+    }
+  ).catch(err => callback(err));
+
+}
+  
 exports.handler = predict;
+  
--- src/utils.js
+++ src/utils.js
@@ -1,50 +1,91 @@
-export const ALL_LISTS = 'lists';
-export const ALL_TODOS = 'all';
-export const ACTIVE_TODOS = 'active';
-export const COMPLETED_TODOS = 'completed';
+export const ALL_LISTS = "lists";
+export const ALL_TODOS = "all";
+export const ACTIVE_TODOS = "active";
+export const COMPLETED_TODOS = "completed";
 
 export default {
-		uuid() {
-			/*jshint bitwise:false */
-			var i, random;
-			var uuid = '';
+  uuid() {
+    /*jshint bitwise:false */
+    var i, random;
+    var uuid = "";
 
-			for (i = 0; i < 32; i++) {
-				random = Math.random() * 16 | 0;
-				if (i === 8 || i === 12 || i === 16 || i === 20) {
-					uuid += '-';
-				}
-				// eslint-disable-next-line
-				uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random))
-					.toString(16);
-			}
+    for (i = 0; i < 32; i++) {
+      random = (Math.random() * 16) | 0;
+      if (i === 8 || i === 12 || i === 16 || i === 20) {
+        uuid += "-";
+      }
+      // eslint-disable-next-line
+      uuid += (i === 12 ? 4 : i === 16 ? (random & 3) | 8 : random).toString(
+        16
+      );
+    }
 
-			return uuid;
-		},
+    return uuid;
+  },
 
-		pluralize (count, word) {
-			return count === 1 ? word : word + 's';
-		},
+  pluralize(count, word) {
+    return count === 1 ? word : word + "s";
+  },
 
-		store (namespace, data) {
-			if (data) {
-				return localStorage.setItem(namespace, JSON.stringify(data));
-			}
+  store(namespace, data) {
+    if (data) {
+      return localStorage.setItem(namespace, JSON.stringify(data));
+    }
 
-			var store = localStorage.getItem(namespace);
-			return (store && JSON.parse(store)) || [];
-		},
+    var store = localStorage.getItem(namespace);
+    return (store && JSON.parse(store)) || [];
+  },
 
-		extend () {
-			var newObj = {};
-			for (var i = 0; i < arguments.length; i++) {
-				var obj = arguments[i];
-				for (var key in obj) {
-					if (obj.hasOwnProperty(key)) {
-						newObj[key] = obj[key];
-					}
-				}
-			}
-			return newObj;
-		}
-	};
+  extend() {
+    var newObj = {};
+    for (var i = 0; i < arguments.length; i++) {
+      var obj = arguments[i];
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          newObj[key] = obj[key];
+        }
+      }
+    }
+    return newObj;
+  },
+
+  searchClarify: file => {
+		console.log(file)
+    window.fetch(
+      `/.netlify/functions/clarifai`,
+      {
+				method: "POST",
+				// type: 'image/json',
+        body: file
+      }
+    ).then(res => {
+			console.log('resing', res)
+			return res.json()
+		}).then(json => console.log(json)).catch(err => console.log(err))
+		// const res = await response.json();
+		// console.log(res)
+		// return res
+  }
+};
+
+// getUrl: async file => {
+// 	var reader = new FileReader();
+// 	reader.addEventListener('loadend', ()=> {
+
+// 		const response = await window.fetch(
+// 			`https://f6qcg6chj5.execute-api.us-east-1.amazonaws.com/dev/requestUploadURL`,
+// 			{
+// 				method: "POST",
+// 				body: new Blob([reader.result], { type: file.type })
+//     }
+// 		);
+// 		return await response.json();
+// 	})
+// },
+// upload: async (url, file) => {
+//   const ack = await window.fetch(url, {
+//     method: "PUT",
+//     body: file
+//   });
+//   console.log(ack);
+// }
