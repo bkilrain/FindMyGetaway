c3bd6ca69a87ad5167ce90c3f056e308b92e3349
Tia Esguerra <msksfo@gmail.com>
2018-11-04 13:42:48 -0800

    Add fileInput component and corresponding styles to landing page

--- src/landing.js
+++ src/landing.js
@@ -13,9 +13,10 @@ const LandingPage = () => {
                 <h1 className="title"><span className="titleTop">Find My</span> <span className="titleBottom">Getaway</span></h1>
 
                 <p className="appDescription">Find My Getaway allows you to upload a photo that inspires you to travel. The app will then suggest a vacation package based off of your photo. We have packages from all seven continents. Whether you are looking for an active vacation, indulgent relaxation, or anything in between, we have you covered. Get started today and embrace your wanderlust.</p>
+
+                {renderFileUpload()}
             </div>
 
-            {renderFileUpload()}
         </div>
     );
 };
