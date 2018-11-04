83ffbfe5b7bcea99ed1a65085c1026ae6376e7c1
Tia Esguerra <msksfo@gmail.com>
2018-11-04 13:00:56 -0800

    Move file upload component to the landing page.

--- src/landing.js
+++ src/landing.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./landing.css";
+import FileUpload from "./fileUpload";
 
 const LandingPage = () => {
+    const renderFileUpload = () => {
+        return <FileUpload />;
+    }
+
     return (
         <div className="landingPage">
             <div className="overlay">
@@ -9,6 +14,8 @@ const LandingPage = () => {
 
                 <p className="appDescription">Find My Getaway allows you to upload a photo that inspires you to travel. The app will then suggest a vacation package based off of your photo. We have packages from all seven continents. Whether you are looking for an active vacation, indulgent relaxation, or anything in between, we have you covered. Get started today and embrace your wanderlust.</p>
             </div>
+
+            {renderFileUpload()}
         </div>
     );
 };
