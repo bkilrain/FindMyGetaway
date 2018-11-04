0ecc6bb3470fc415883879862e4a9c3cb7177872
Tia Esguerra <msksfo@gmail.com>
2018-11-04 12:48:51 -0800

    Create initial landing page styles

---
+++ src/landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./landing.css";
+
+const LandingPage = () => {
+    return (
+        <div className="landingPage">
+            <div className="overlay">
+                <h1 className="title"><span className="titleTop">Find My</span> <span className="titleBottom">Getaway</span></h1>
+
+                <p className="appDescription">Find My Getaway allows you to upload a photo that inspires you to travel. The app will then suggest a vacation package based off of your photo. We have packages from all seven continents. Whether you are looking for an active vacation, indulgent relaxation, or anything in between, we have you covered. Get started today and embrace your wanderlust.</p>
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
