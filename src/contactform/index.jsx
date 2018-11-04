f7cce6f787364458ceebb88437fb73f8bc473449
Perry <perry@f35.com>
2018-11-03 20:41:14 -0700

    return to working order

--- src/contactform/index.jsx
+++ src/contactform/index.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const ContactForm = () => {
   return (
     <form>
-        <input type="text" name="name"/>
-        <input type="text" name="email"/>
-        <input type="text" name="message"/>
+            <input type="text" name="name"/>
+            <input type="text" name="email"/>
+            <input type="text" name="message"/>
     </form>
   );
 };
